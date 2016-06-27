7d47e4dbdc68c033ba73a432776c2ae4aeea7ed8
DG777 <latooma@gmail.com>
2016-06-28 09:40:12 +1200

    Moving bg unccoment

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -42,15 +42,15 @@ animateApp.config(function ($routeProvider) {
 //home page controller
 animateApp.controller('homeController', function ($scope) {
     $scope.pageClass = "page-home";
-//    $(function () {
-//        var x = 0;
-//        var y = 0;
-//        setInterval(function () {
-//            x += 1.16;
-//            y -= 1;
-//            $('body').css('background-position', x + 'px ' + y + 'px');
-//        }, 40);
-//    });
+    $(function () {
+        var x = 0;
+        var y = 0;
+        setInterval(function () {
+            x += 1.16;
+            y -= 1;
+            $('body').css('background-position', x + 'px ' + y + 'px');
+        }, 40);
+    });
 });
 
 //construction page controller
