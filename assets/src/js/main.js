7a15e8805984fde02f602d33d26bbc977d5272be
DG777 <latooma@gmail.com>
2016-06-28 11:17:43 +1200

    Fixed bg flicker, indented docs, removed some comments

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,29 +1,3 @@
-//$(function () {
-//    var x = 0;
-//    var y = 0;
-//    setInterval(function () {
-//        x += 1.16;
-//        y -= 1;
-//        $('body').css('background-position', x + 'px ' + y + 'px');
-//    }, 40);
-//});
-
-
-$(document).ready(function () {
-    $(function () {
-        var x = 0;
-        var y = 0;
-        setInterval(function () {
-            x += 1.16;
-            y -= 1;
-            $('body').css('background-position', x + 'px ' + y + 'px');
-        }, 40);
-    });
-    $("#confirm_build").on('click', function () {
-        $('body').css('background-position', x + 'px ' + y + 'px');
-    });
-})
-
 //Building screen buttons and toggles
 
 $(function () {
